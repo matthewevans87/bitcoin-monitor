8290bfeb3f8d1f2b76e657c0ea76e9db17bde74c
Matthew Evans <matt@matthewevansconsulting.com>
2018-09-30 21:51:17 -0500

    adding href for link to git repo

--- CoinbaseTelemetryMonitor/Startup.cs
+++ CoinbaseTelemetryMonitor/Startup.cs
@@ -68,10 +68,10 @@ namespace CoinbaseTelemetryMonitor
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
-                //if (env.IsDevelopment())
-                //{
-                //    spa.UseAngularCliServer(npmScript: "start");
-                //}
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
 
             });
         }
